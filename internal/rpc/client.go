23cb69a0c649c6855dcb8344dd88bb07aec6cf3a
codewithzubair07 <juberquraishi553@gmail.com>
2026-02-26 02:47:10 +0530

    refactor: abstract core HTTP client behind interface (closes #603)

--- internal/rpc/client.go
+++ internal/rpc/client.go
@@ -104,7 +104,7 @@ type Client struct {
 	AltURLs      []string
 	currIndex    int
 	mu           sync.RWMutex
-	httpClient   *http.Client
+	httpClient   httpclient.HTTPClient
 	token        string // stored for reference, not logged
 	Config       NetworkConfig
 	CacheEnabled bool
@@ -247,7 +247,7 @@ func (c *Client) rotateURL() bool {
 	return true
 }
 
-func (c *Client) getHTTPClient() *http.Client {
+func (c *Client) getHTTPClient() httpclient.HTTPClient {
 	if c.httpClient != nil {
 		return c.httpClient
 	}
@@ -255,7 +255,7 @@ func (c *Client) getHTTPClient() *http.Client {
 }
 
 // createHTTPClient creates an HTTP client with optional authentication and a configurable timeout.
-func createHTTPClient(token string, timeout time.Duration) *http.Client {
+func createHTTPClient(token string, timeout time.Duration) httpclient.HTTPClient {
 	cfg := DefaultRetryConfig()
 
 	var baseTransport http.RoundTripper = http.DefaultTransport
